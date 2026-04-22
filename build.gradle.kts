3871c7887c03fac51fceae3f2ed82b1bff633fdd
EpsteinKim <yskim9718@naver.com>
2026-04-22 16:31:04 +0900

    fix: 루트 build.gradle.kts 불필요한 dependencies/repositories 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     kotlin("jvm") version "2.2.21" apply false
-    kotlin("plugin.spring") version "2.2.21" apply false
     id("org.springframework.boot") version "4.0.5" apply false
     id("io.spring.dependency-management") version "1.1.7" apply false
     kotlin("plugin.jpa") version "2.2.21" apply false
     kotlin("kapt") version "2.2.21" apply false
+    kotlin("plugin.spring") version "2.2.21"
 }
 
 allprojects {
